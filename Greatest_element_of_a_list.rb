f15a0f5eff30f1fa8d6c01d2d3aefb2694b38e8d
Ben Bubnick <ben.bubnick@gmail.com>
2015-06-16 07:45:47 -0400

    Create Greatest_element_of_a_list.rb

---
+++ Greatest_element_of_a_list.rb
@@ -0,0 +1,4 @@
+#Create a function that returns the maximum value in a provided set of values, where the number of values may not be known until runtime.
+
+#max is a method of all Enumerables
+values.max
