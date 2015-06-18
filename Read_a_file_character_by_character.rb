8d295d84bc9188a2adf35a90ad317328950d10b2
Ben Bubnick <ben.bubnick@gmail.com>
2015-06-18 15:05:01 -0400

    Update Read_a_file_character_by_character.rb

--- Read_a_file_character_by_character.rb
+++ Read_a_file_character_by_character.rb
@@ -1,5 +1,13 @@
+
+File.open('input.txt', 'r:utf-8') do |f|
+  f.each_char{|c| p c}
+end
+
+=begin
+#old style of doing this
 File.open('input.txt', 'r:utf-8') do |f|
   while c = f.getc
     p c
   end
 end
+=end
